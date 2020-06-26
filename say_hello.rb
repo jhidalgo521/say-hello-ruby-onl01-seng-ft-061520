4c00297ab2c43a48d368c8f8617dc9fc242d48db
Jose Hidago <jhidalgo521@gmail.com>
2020-06-26 18:06:33 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,5 @@
 # Build your say_hello method here
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
@@ -6,3 +7,8 @@ end
 
 
 
+=======
+def say_hello(name)
+  puts "Hello #{name}!"
+end
+>>>>>>> 2f16b507e7a3e1b20bb2f4c1681beb3f7981f069
